f63b4cfdb49b5ff0c8ac7e1dfb9b517b8f1b944f
6ober <31521481+6ober@users.noreply.github.com>
2020-02-22 16:14:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,4 +10,5 @@ elif message.text == "/help":
     bot.send_message(message.from_user.id, "Напиши привет")
 else:
     bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
 bot.polling(none_stop=True, interval=0)
