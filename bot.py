dc0df4152e5dc5def000db07946d021774873adf
6ober <31521481+6ober@users.noreply.github.com>
2020-02-22 16:10:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,12 @@ import telebot;
 from telebot import types
 
 bot = telebot.TeleBot('1005817495:AAH1JlVfSCb3M4C6GcrLvZl_VvDWTcK3Evc');
-
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-
 if message.text == "Привет":
     bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
 elif message.text == "/help":
     bot.send_message(message.from_user.id, "Напиши привет")
 else:
     bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-
 bot.polling(none_stop=True, interval=0)
